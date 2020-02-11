b910b99f881e8660a350565e92dd3e7162880220
Gopolang <Gopolang@Lenovo-PC>
2020-02-11 22:18:52 +0200

    created packages and formated the structure

---
+++ src/main/java/com/gopolangmathole/amazingsauce/AmazingSauceApplication.java
@@ -0,0 +1,13 @@
+package com.gopolangmathole.amazingsauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingSauceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingSauceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gopolangmathole/amazingsauce/AmazingSauceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopolangmathole.amazingsauce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingSauceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
